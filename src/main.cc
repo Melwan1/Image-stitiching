11fc786adb2774e65d9f3090b7c11d376cca17e0
Melwan Chevassus <melwan.chevassus@epita.fr>
2025-07-04 16:20:05 +0200

    fix(main): add distance testing with initial image

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,6 @@
 #include <images/ppm-image.hh>
+#include <iostream>
+#include <metrics/distance/image_distance.hh>
 #include <panorama/builder/overlap_rectangular_builder.hh>
 #include <panorama/cutter/overlap_rectangular_cutter.hh>
 #include <sstream>
@@ -11,10 +13,9 @@ int main()
     cutter.set_input_image(image);
     cutter.set_horizontal_slices(3)
         .set_vertical_slices(6)
-        .set_horizontal_overlap_size(15)
-        .set_vertical_overlap_size(15);
+        .set_horizontal_overlap_size(10)
+        .set_vertical_overlap_size(20);
     std::vector<tifo::image::Image*> cut_images = cutter.cut();
-    cutter.free_input();
     int index = 1;
     for (const auto cut_image : cut_images)
     {
@@ -29,11 +30,15 @@ int main()
     tifo::image::Image* built_image = builder.build();
     built_image->write("tests/result.ppm");
 
-    /*tifo::panorama::builder::CleanRectangularBuilder builder;
-    builder.set_input_images(cut_images);
-    builder.set_horizontal_slices(2).set_vertical_slices(4);
-    tifo::image::Image* built_image = builder.build();
-    builder.free_inputs();
-    built_image->write("tests/new_julie.ppm");*/
+    tifo::metrics::distance::ImageDistance image_distance;
+    std::cout << "built image: " << built_image->get_width() << "x"
+              << built_image->get_height() << "\n";
+    std::cout << "initial image: " << image->get_width() << "x"
+              << image->get_height() << "\n";
+    image_distance.set_input_images({ built_image, image });
+    double distance = image_distance.compute_distance();
+    std::cout << "cut distance with respect to original image: " << distance
+              << "\n";
+
     return 0;
 }
